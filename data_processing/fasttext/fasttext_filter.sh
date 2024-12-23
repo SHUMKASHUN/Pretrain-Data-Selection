117f37ca5fbddcb499468730b9b918436cf214ed
SHUMKASHUN <ksshumab@connect.ust.hk>
2024-12-24 07:26:55 +0800

    update data processing to support percentage argument

--- data_processing/fasttext/fasttext_filter.sh
+++ data_processing/fasttext/fasttext_filter.sh
@@ -7,27 +7,27 @@ FASTTEXT_MODEL_PATH=$2  ## /university/Pretrain/datatrove/Fasttext-Model/
 POOL_PATH=$3 # /university/DCLM-refinedweb/DCLM-refinedweb-400M-fasttext-pool-80B/
 OUTPUT_PATH=$4 # /university/DCLM-refinedweb/${MODEL}
 FASTTEXT_LABEL=$5 
+PERCENTAGE=$6
+
 if [ ! -f ${FASTTEXT_MODEL_PATH}/${FASTTEXT_MODEL_NAME}.bin ]; then
     echo "Fasttext Model ${FASTTEXT_MODEL_NAME} not found in ${FASTTEXT_MODEL_PATH}, start downloading"
-    wget https://huggingface.co/ksshumab/cluster/resolve/main/${FASTTEXT_MODEL_NAME}.bin?download=true -O ${FASTTEXT_MODEL_PATH}${FASTTEXT_MODEL_NAME}.bin
+    wget https://hf-mirror.com/ksshumab/cluster/resolve/main/${FASTTEXT_MODEL_NAME}.bin?download=true -O ${FASTTEXT_MODEL_PATH}${FASTTEXT_MODEL_NAME}.bin
 else
     echo "Fasttext Model ${FASTTEXT_MODEL_NAME} already exist in ${FASTTEXT_MODEL_PATH}, skip downloading"
 fi
 
 
-# python filter.py --fasttext  ${FASTTEXT_MODEL_PATH}${MODEL} --output_name  /university/DCLM-refinedweb/${MODEL} 
-#python filter.py --fasttext  ${MODEL} --output_name  dclm_original --threshold 0.026
 
 echo "Begin scoring for each docs"
 conda run --live-stream -n datatrove python ./data_processing/fasttext/filter.py --input_path ${POOL_PATH}\
     --fasttext  ${FASTTEXT_MODEL_PATH}${FASTTEXT_MODEL_NAME}\
-    --output_path ${OUTPUT_PATH}${FASTTEXT_MODEL_NAME}\
+    --output_path ${OUTPUT_PATH} \
     --label_name ${FASTTEXT_LABEL}\
     --threshold -1000
 
-echo "Finish scoring for each docs, finding a threshold correspond to top 10% data..."
+echo "Finish scoring for each docs, finding a threshold correspond to top ${PERCENTAGE} data..."
 
-THRESHOLD=$(python ./data_processing/fasttext/find_threshold.py --data_path  ${OUTPUT_PATH}${FASTTEXT_MODEL_NAME} --label_name ${FASTTEXT_LABEL})
+THRESHOLD=$(python ./data_processing/fasttext/find_threshold.py --data_path  ${OUTPUT_PATH} --label_name ${FASTTEXT_LABEL} --percentage ${PERCENTAGE})  
 echo ${THRESHOLD}
 
 THRESHOLD=$(echo ${THRESHOLD} | rev | cut -d' ' -f 1| rev)
@@ -35,7 +35,7 @@ echo "Find the new threshold:  ${THRESHOLD}"
 
 conda run --live-stream -n datatrove python ./data_processing/fasttext/filter.py --input_path ${POOL_PATH}\
     --fasttext  ${FASTTEXT_MODEL_PATH}${FASTTEXT_MODEL_NAME}\
-    --output_path ${OUTPUT_PATH}${FASTTEXT_MODEL_NAME}\
+    --output_path ${OUTPUT_PATH} \
     --label_name ${FASTTEXT_LABEL} \
     --threshold ${THRESHOLD}
 
--- data_processing/fasttext/find_threshold.py
+++ data_processing/fasttext/find_threshold.py
@@ -5,6 +5,7 @@ import numpy as np
 parser = argparse.ArgumentParser("Filter an HF dataset and push the result to the hub")
 parser.add_argument("--data_path",type=str, help="data path name")
 parser.add_argument("--label_name",type=str, help="output name")
+parser.add_argument("--percentage",type=float, help="top how many percent", default=0.1)
 
 
 if __name__ == "__main__":
@@ -22,11 +23,11 @@ if __name__ == "__main__":
         print(f"file {i} read finished")
         scores_and_length.sort(key=lambda x: x[0], reverse=True)
         current_sum = 0
-        for i in range(0, len(scores_and_length)):
-            current_sum += scores_and_length[i][1]
-            if current_sum > 0.1 * total_length:
-                print(f"10% length threshold score: {scores_and_length[i][0]}, thre is total # {i} docs, averaged length: {current_sum/(i+1)}")
-                print_score = scores_and_length[i][0]
+        for j in range(0, len(scores_and_length)):
+            current_sum += scores_and_length[j][1]
+            if current_sum > args.percentage * total_length:
+                print(f"{args.percentage} length threshold score: {scores_and_length[j][0]}, thre is total # {j} docs, averaged length: {current_sum/(j+1)}")
+                print_score = scores_and_length[j][0]
                 break
     print(print_score)
             # if sum([x[1] for x in scores_and_length[:i]]) > 0.1 * total_length:
